3b7320cfceb0d0c0d75ab8f888275ed941503810
mohakjl <mohamedkoujil75@gmail.com>
2024-10-19 00:37:41 +0200

    adSense script

--- src/components/AdComponent.vue
+++ src/components/AdComponent.vue
@@ -1,4 +1,38 @@
-<script setup></script>
-<template></template>
+<template>
+  <div>
+    <ins
+      class="adsbygoogle"
+      style="display: block"
+      data-ad-client="ca-pub-XXXXXXXXXX"
+      <!--
+      Reemplaza
+      con
+      tu
+      ID
+      de
+      AdSense
+      --
+    >
+      data-ad-slot="1234567890"
+      <!-- Reemplaza con tu slot de anuncio -->
+      data-ad-format="auto" data-full-width-responsive="true" ></ins
+    >
+  </div>
+</template>
 
-<style scoped></style>
+<script>
+export default {
+  mounted() {
+    const script = document.createElement("script");
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    script.async = true;
+    script.setAttribute("data-ad-client", "ca-pub-XXXXXXXXXX"); // Reemplaza con tu ID de AdSense
+    document.head.appendChild(script);
+  },
+};
+</script>
+
+<style scoped>
+/* Puedes personalizar el estilo aquí si es necesario */
+</style>
